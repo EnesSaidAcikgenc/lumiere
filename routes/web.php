6803338f2f8e7ec8a9bacca10d0c102e945cda93
EnesSaidAcikgenc <110100794+EnesSaidAcikgenc@users.noreply.github.com>
2025-09-17 23:25:39 +0300

    routlar düzeltildi

--- routes/web.php
+++ routes/web.php
@@ -22,35 +22,35 @@ use Illuminate\Support\Facades\Route;
 
 //--------------------------------MENU SAYFASI ROUTELARI BAŞLANGIÇ---------------------------------------
 
-Route::get('/', [MenuController::class,'index'])->name('home');
-Route::get('/menu/{id}', [MenuController::class,'menu'])->name('menu');
+Route::get('/', [MenuController::class,'indexffffffff'])->name('home');
+Route::get('/menu/{id}', [MenuController::class,'menuffffffff'])->name('menu');
 
 //-------------------------------MENU SAYFASI ROUTELARI BİTİŞ-----------------------------------------------
 
 
 
 
-Route::get('/login', [LoginController::class,'login'])->name('login');
-Route::post('/loginpost', [LoginController::class,'loginp'])->name('loginp');
-Route::get('/logout', [LoginController::class,'logout'])->name('logout');
+Route::get('/login', [LoginController::class,'loginffffffff'])->name('login');
+Route::post('/loginpost', [LoginController::class,'loginpffffffff'])->name('loginp');
+Route::get('/logout', [LoginController::class,'logoutffffffff'])->name('logout');
 
 
 
 
 //----------------------------------ADMİN SAYFASI ROUTELARI BAŞLANGIÇ-------------------------------------------
 Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin',[AdminController::class,'index'])->name('admin');
-    Route::get('/kategoriler',[AdminController::class,'categories'])->name('categorie');
-    Route::get('/kategoriEkleme',[AdminController::class,'add_categories'])->name('add_categories');
-    Route::post('/kategoriEkle',[AdminController::class,'category_add'])->name('category_add');
-    Route::get('/kategoriGüncelleme/{id}',[AdminController::class,'edit_categories'])->name('edit_categories');
-    Route::post('/kategoriGüncelle',[AdminController::class,'category_update'])->name('category_update');
-    Route::get('/kategoriSilme/{id}',[AdminController::class,'delete_categories'])->name('delete_categories');
-    Route::get('/ürünler',[AdminController::class,'products'])->name('products');
-    Route::get('/ürünEkleme',[AdminController::class,'add_product'])->name('add_product');
-    Route::post('/ürünEkle',[AdminController::class,'product_add'])->name('product_add');
-    Route::get('/ürünGüncelleme/{id}',[AdminController::class,'edit_product'])->name('edit_product');
-    Route::post('/ürünGüncelle',[AdminController::class,'products_update'])->name('products_update');
-    Route::get('/ürünSilme/{id}',[AdminController::class,'delete_product'])->name('delete_product');
+    Route::get('/admin',[AdminController::class,'indexffffffff'])->name('admin');
+    Route::get('/kategoriler',[AdminController::class,'categoriesffffffff'])->name('categorie');
+    Route::get('/kategoriEkleme',[AdminController::class,'add_categoriesffffffff'])->name('add_categories');
+    Route::post('/kategoriEkle',[AdminController::class,'category_addffffffff'])->name('category_add');
+    Route::get('/kategoriGüncelleme/{id}',[AdminController::class,'edit_categoriesffffffff'])->name('edit_categories');
+    Route::post('/kategoriGüncelle',[AdminController::class,'category_updateffffff'])->name('category_update');
+    Route::get('/kategoriSilme/{id}',[AdminController::class,'delete_categoriesffffff'])->name('delete_categories');
+    Route::get('/ürünler',[AdminController::class,'productsffffff'])->name('products');
+    Route::get('/ürünEkleme',[AdminController::class,'add_productffffff'])->name('add_product');
+    Route::post('/ürünEkle',[AdminController::class,'product_addffffff'])->name('product_add');
+    Route::get('/ürünGüncelleme/{id}',[AdminController::class,'edit_productffffff'])->name('edit_product');
+    Route::post('/ürünGüncelle',[AdminController::class,'products_updateffffff'])->name('products_update');
+    Route::get('/ürünSilme/{id}',[AdminController::class,'delete_productffffff'])->name('delete_product');
 });
 //----------------------------------ADMİN SAYFASI ROUTELARI BİTİŞ-------------------------------------------
